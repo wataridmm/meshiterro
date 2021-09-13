b4ab8ea6e2aca4db6cad2143e537564d6b4374b6
wataridmm <r.watari.watari@gmail.com>
2021-09-13 11:25:30 +0000

    add error?

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,12 +1,21 @@
 class PostImagesController < ApplicationController
   
+  
   def new
+     @post_image = PostImage.new
+
   end
 
+　　# 投稿データの保存
   def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
+     @post_images = PostImage.all
   end
 
   def show
@@ -15,5 +24,11 @@ class PostImagesController < ApplicationController
   def destroy
   end
   
+ 　　 # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
   
 end
